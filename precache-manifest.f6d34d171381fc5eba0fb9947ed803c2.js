e164fe605232368da00e887341fc77ac5969d5c0
posssible99 <juanpsfi3023@gmail.com>
2020-06-21 20:01:47 -0500

    Updates

---
+++ precache-manifest.f6d34d171381fc5eba0fb9947ed803c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc23257c31c94e3506d0b11e72966981",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "331ff19b9c1ad460e548",
+    "url": "/monsters-rolodex/static/css/main.0a52f99b.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "331ff19b9c1ad460e548",
+    "url": "/monsters-rolodex/static/js/main.b72feff1.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
